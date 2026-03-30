8aa147c2981de8d9b6aa5894e7c589e5db8281e6
NielsSchool123 <nielsvanderpijl023@gmail.com>
2026-03-30 11:19:43 +0200

    Add SudokuActivity and link home button

--- app/src/main/java/com/example/minigamesapp/MainActivity.java
+++ app/src/main/java/com/example/minigamesapp/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.minigamesapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // 1. Find the button by the ID we just gave it in the XML
+        Button startSudokuBtn = findViewById(R.id.btn_start_sudoku);
+
+        // 2. Set an OnClickListener to trigger when the button is tapped
+        startSudokuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 3. Create an Intent to navigate from this activity to the SudokuActivity
+                Intent intent = new Intent(MainActivity.this, SudokuActivity.class);
+
+                // 4. Start the new activity
+                startActivity(intent);
+            }
         });
     }
 }
