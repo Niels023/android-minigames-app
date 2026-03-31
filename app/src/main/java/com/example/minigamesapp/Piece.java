f5da8280fb016db98acf39777d43749bf6ac0ee8
Kick Groen <kick.groentt@gmail.com>
2026-03-31 18:20:53 +0200

    Added Piece Class

---
+++ app/src/main/java/com/example/minigamesapp/Piece.java
@@ -0,0 +1,11 @@
+package com.example.minigamesapp;
+
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+public class Piece {
+    public boolean isWhite;
+    public Position position;
+    public String pictureLocation;
+    public ImageView chessImage;
+}
