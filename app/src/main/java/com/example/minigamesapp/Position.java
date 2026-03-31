8c23e2213d8d25f0807b14f5f0be1a6adf2b48c9
Kick Groen <kick.groentt@gmail.com>
2026-03-31 18:20:26 +0200

    Added Position Class

---
+++ app/src/main/java/com/example/minigamesapp/Position.java
@@ -0,0 +1,11 @@
+package com.example.minigamesapp;
+
+public class Position {
+    public int row;
+    public int column;
+
+    public Position (int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+}
