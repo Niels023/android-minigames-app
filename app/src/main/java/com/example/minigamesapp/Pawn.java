06da883c9183be6ef408d2206bf86398121352e2
Kick Groen <kick.groentt@gmail.com>
2026-04-02 16:10:34 +0200

    Update Pawn.java
    Removed board parameter from isMoveLegal()
    Changed getLegalMoves() parameter board from GridLayout to Board

--- app/src/main/java/com/example/minigamesapp/Pawn.java
+++ app/src/main/java/com/example/minigamesapp/Pawn.java
@@ -1,10 +1,15 @@
 package com.example.minigamesapp;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 
 import org.intellij.lang.annotations.Identifier;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Pawn extends Piece implements IPiece {
 
@@ -20,28 +25,58 @@ public class Pawn extends Piece implements IPiece {
 
         chessImage.setImageResource(pictureLocation);
     }
-
-    public boolean isMoveLegal(Position targetPos){
+    public boolean isMoveLegal(Board board, Position targetPos){
         int firstRow;
         int secondRow;
         if (this.isWhite) {
-            firstRow = 1;
-            secondRow = 2;
-        } else {
             firstRow = -1;
             secondRow = -2;
+        } else {
+            firstRow = 1;
+            secondRow = -2;
         }
-
         Position pawnPos = this.position;
         // Check for front moves
         if(targetPos.column == pawnPos.column) {
             // same column
             if (targetPos.row - pawnPos.row == firstRow) {
                 // Position is 1 in front
+                ImageView square = (ImageView) board.gridLayout.getChildAt(targetPos.row * 8 + targetPos.column);
+                Drawable drawable = square.getDrawable();
+                boolean hasImage = (drawable != null);
 
+                if (!hasImage) {
+                    return true;
+                }
+
+                Piece piece = board.getPieceFromPosition(targetPos);
+
+                Log.e("e", Boolean.toString(piece.isWhite));
+                if (piece.isWhite != this.isWhite) {
+                    return true;
+                } else {
+                    return false;
+                }
             } else if (targetPos.row - pawnPos.row == secondRow) {
                 // Position is 2 in front
-                return true;
+                ImageView square = (ImageView) board.gridLayout.getChildAt(targetPos.row * 8 + targetPos.column);
+                Drawable drawable = square.getDrawable();
+                boolean hasImage = (drawable != null);
+
+                if (!hasImage) {
+                    return true;
+                }
+
+                Piece piece = board.getPieceFromPosition(targetPos);
+                if (piece == null) {
+                    return true;
+                }
+                Log.e("e", Boolean.toString(piece.isWhite));
+                if (piece.isWhite != this.isWhite) {
+                    return true;
+                } else {
+                    return false;
+                }
             }
         } else {
             // Check for diagonal takes
@@ -49,11 +84,17 @@ public class Pawn extends Piece implements IPiece {
         }
         return false;
     };
-
-    public Position[] getLegalMoves(GridLayout board, Piece piece) {
-        for (int i = 0; i < board.getChildCount(); i++) {
-            ImageView
+    public Position[] getLegalMoves(Board board, Piece piece) {
+        List<Position> list = new ArrayList<>();
+        for (int index = 0; index < board.gridLayout.getChildCount(); index++) {
+            if (isMoveLegal(board, getPositionFromIndex(index))) {
+                list.add(getPositionFromIndex(index));
+                Log.e("IsLegal", getPositionFromIndex(index).row + "," + getPositionFromIndex(index).column + " is a legal move!");
+            } else {
+                Log.e("IsNOTLegal", getPositionFromIndex(index).row + "," + getPositionFromIndex(index).column + " is NOT a legal move!");
+            }
         }
+        return list.toArray(new Position[0]);
     }
     public void move(Position targetPosition){
 
