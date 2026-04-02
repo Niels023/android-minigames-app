f7f44c1108e53a5408856d8c496e15e1066d46ab
Kick Groen <kick.groentt@gmail.com>
2026-04-02 12:00:38 +0200

    Added setup function

--- app/src/main/java/com/example/minigamesapp/Board.java
+++ app/src/main/java/com/example/minigamesapp/Board.java
@@ -5,10 +5,16 @@ import android.graphics.Color;
 import android.util.Log;
 import android.view.View;
 import android.widget.GridLayout;
+import android.widget.ImageView;
 
 public class Board{
 
-    public GridLayout boardGrid;
+    public GridLayout gridLayout;
+    public Piece[] pieces;
+
+    public Board(GridLayout grid) {
+        gridLayout = grid;
+    }
 
     public void addPiece(Piece chessPiece) {
         Position pos = chessPiece.position;
@@ -16,7 +22,33 @@ public class Board{
                 GridLayout.spec(pos.row),
                 GridLayout.spec(pos.column)
         );
-        boardGrid.addView(chessPiece.chessImage, params);
+        gridLayout.addView(chessPiece.chessImage, params);
         Log.e("Placed", "Piece has been added to: " + pos.row + "," + pos.column);
+    }
+
+    public void setup(Context context) {
+        boolean colorIsBlack = false;
+        int squareSize = (int)(46.875 * context.getResources().getDisplayMetrics().density); // turning 50dp into pixels cuz the layout params only accept pixels!!
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                ImageView square = new ImageView(context);
+
+                square.setBackgroundColor(colorIsBlack ? Color.rgb(182, 137, 99) : Color.rgb(240, 218, 182));
+
+                GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+                params.width = squareSize;
+                params.height = squareSize;
+                params.rowSpec = GridLayout.spec(row);
+                params.columnSpec = GridLayout.spec(col);
+                square.setLayoutParams(params);
+
+                square.setId(ImageView.generateViewId());
+
+                gridLayout.addView(square);
+
+                colorIsBlack = !colorIsBlack;
+            }
+            colorIsBlack = !colorIsBlack;
+        }
     }
 }
