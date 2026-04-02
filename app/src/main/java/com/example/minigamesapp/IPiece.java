1a9b2ea090b729bc5c57b75b4d3632a60c1260e6
Kick Groen <kick.groentt@gmail.com>
2026-04-02 16:09:29 +0200

    added IPiece to Piece

--- app/src/main/java/com/example/minigamesapp/IPiece.java
+++ app/src/main/java/com/example/minigamesapp/IPiece.java
@@ -3,5 +3,6 @@ package com.example.minigamesapp;
 public interface IPiece {
     boolean isMoveLegal(Position targetPosition);
     void move(Position targetPosition);
+    Position[] getLegalMoves(Board board, Piece piece);
 
 }
--- app/src/main/java/com/example/minigamesapp/Piece.java
+++ app/src/main/java/com/example/minigamesapp/Piece.java
@@ -1,11 +1,30 @@
 package com.example.minigamesapp;
 
+import android.util.Log;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 
-public class Piece {
+public class Piece implements IPiece{
     public boolean isWhite;
     public Position position;
-    public String pictureLocation;
     public ImageView chessImage;
+    public Position getPositionFromIndex(int index) {
+        int moduloRow = 0;
+        while (index >= 8) {
+            index -= 8;
+            moduloRow++;
+        }
+        int moduloCol = index; // left-overs
+        return new Position(moduloRow, moduloCol);
+    }
+    public boolean isMoveLegal(Position targetPosition){
+        Log.e("OH NO", "no work");
+        return false;
+    };
+    public void move(Position targetPosition){
+    };
+    public Position[] getLegalMoves(Board board, Piece piece) {
+        Log.e("OH NO", "no work");
+        return null;
+    };
 }
