7efd8c7590efe3a4423f55661b777042b1ce72c1
Kick Groen <kick.groentt@gmail.com>
2026-04-13 11:28:26 +0200

    Update Rook.java
    Finished rook logic.
    Changed method getAllMoves() so it doesn't go through all 64 squares since a rook does NOT need to go check through 64 squares if they're legal. only the ones on the same row and column as it.

--- app/src/main/java/com/example/minigamesapp/Rook.java
+++ app/src/main/java/com/example/minigamesapp/Rook.java
@@ -1,5 +1,7 @@
 package com.example.minigamesapp;
 
+import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -17,21 +19,115 @@ public class Rook extends Piece implements IPiece {
         chessImage.setImageResource(pictureLocation);
     }
 
-    public boolean isMoveLegal(Board board, Position targetPost) {
-        return false;
+    public boolean isMoveLegal(Board board, Position targetPos) {
+        int currentRow = this.position.row;
+        int currentColumn = this.position.column;
+        int targetRow = targetPos.row;
+        int targetColumn = targetPos.column;
+
+        if (currentRow != targetRow && currentColumn != targetColumn) {
+            return false;
+        }
+
+        if (currentRow == targetRow) {
+            // on same row
+            int step = (targetColumn > currentColumn) ? 1 : -1; // switch 1 and -1 if target column number is higher
+            for (int col = currentColumn + step; col != targetColumn; col += step) {
+                if (board.getPieceFromPosition(new Position(currentRow, col)) != null) {
+                    return false; // piece is in the way
+                }
+            }
+        }
+        else {
+            int step = (targetRow > currentRow) ? 1 : -1;  // switch 1 and -1 if target row number is higher
+
+            for (int row = currentRow + step; row != targetRow; row += step) {
+                if (board.getPieceFromPosition(new Position(row, currentColumn)) != null) {
+                    return false; // piece is in the way
+                }
+            }
+        }
+
+        ImageView square = (ImageView) board.gridLayout.getChildAt(targetRow * 8 + targetColumn);
+        Drawable drawSquare = square.getDrawable();
+        boolean HasImage = (drawSquare != null);
+        if (!HasImage) {
+            return true;
+        }
+
+        Piece targetPiece = board.getPieceFromPosition(targetPos);
+
+        return this.isWhite != targetPiece.isWhite;
     }
 
     public Position[] getLegalMoves(Board board) {
-        List<Position> list = new ArrayList<>();
-        for (int index = 0; index < board.gridLayout.getChildCount(); index++) {
-            if (isMoveLegal(board, getPositionFromIndex(index))) {
-                list.add(getPositionFromIndex(index));
+        List<Position> moves = new ArrayList<>();
+
+        int row = position.row;
+        int col = position.column;
+
+        for (int r = row - 1; r >= 0; r--) {
+            if (!addMoveToList(board, moves, r, col)) {
+                break;
             }
         }
-        return list.toArray(new Position[0]);
+
+        for (int r = row + 1; r < 8; r++) {
+            if (!addMoveToList(board, moves, r, col)) {
+                break;
+            }
+        }
+
+        for (int c = col - 1; c >= 0; c--) {
+            if (!addMoveToList(board, moves, row, c)) {
+                break;
+            }
+        }
+
+        for (int c = col + 1; c < 8; c++) {
+            if (!addMoveToList(board, moves, row, c)) {
+                break;
+            }
+        }
+
+        return moves.toArray(new Position[0]);
     }
 
     public boolean move(Board board, int index) {
-        return false;
+        if (isMoveLegal(board, getPositionFromIndex(index))) {
+            Position pos = getPositionFromIndex(index);
+            ImageView otherView = (ImageView) board.gridLayout.getChildAt(index);
+            ImageView currentView = this.chessImage;
+            currentView.setImageResource(0);
+            otherView.setImageResource(this.pictureLocation);
+            if (board.getPieceFromPosition(pos) != null) {
+                Piece otherPiece = board.getPieceFromPosition(pos);
+                board.pieces.remove(otherPiece);
+            }
+            this.chessImage = otherView;
+            this.position = pos;
+            board.clean();
+            return true;
+        } else {
+            Position pos = getPositionFromIndex(index);
+            Log.e("Pawn", "Illegal Move, " + Integer.toString(this.position.row) + "," + Integer.toString(this.position.column) + " > " + Integer.toString(pos.row) + "," + Integer.toString(pos.column));
+            board.clean();
+            return false;
+        }
+    }
+
+    private boolean addMoveToList(Board board, List<Position> moves, int r, int c) {
+        Position pos = new Position(r, c);
+        Piece piece = board.getPieceFromPosition(pos);
+
+        if (piece == null) {
+            moves.add(pos);
+            return true; // keep going
+        } else {
+            if (piece.isWhite != this.isWhite) {
+                moves.add(pos); // can capture
+            }
+            return false; // stop here (blocked)
+        }
     }
 }
