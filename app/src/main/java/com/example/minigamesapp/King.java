8887da597f84f123718118045f270b67902d00dc
Zen <kick.groentt@gmail.com>
2026-04-16 20:35:59 +0200

    Update King.java
    Updated King Logic

--- app/src/main/java/com/example/minigamesapp/King.java
+++ app/src/main/java/com/example/minigamesapp/King.java
@@ -3,6 +3,9 @@ package com.example.minigamesapp;
 import android.util.Log;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class King extends Piece implements IPiece {
     public int pictureLocation = R.drawable.white_king;
 
@@ -17,11 +20,51 @@ public class King extends Piece implements IPiece {
     }
 
     public boolean isMoveLegal(Board board, Position targetPos) {
-        return super.isMoveLegal(targetPos);
+        int currentRow = position.row;
+        int currentColumn = position.column;
+        int targetRow = targetPos.row;
+        int targetColumn = targetPos.column;
+
+        int rowDiff = Math.abs(currentRow - targetRow);
+        int colDiff = Math.abs(currentColumn - targetColumn);
+
+        // Must move exactly one square in any direction
+        if ((rowDiff <= 1 && colDiff <= 1) && (rowDiff + colDiff != 0)) {
+            Piece targetPiece = board.getPieceFromPosition(targetPos);
+
+            // Allow move if empty or enemy piece
+            return targetPiece == null || targetPiece.isWhite != this.isWhite;
+        }
+
+        return false;
     }
 
     public Position[] getLegalMoves(Board board) {
-        return super.getLegalMoves(board);
+        List<Position> moves = new ArrayList<>();
+
+        int row = position.row;
+        int col = position.column;
+
+        int[] directionRows = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] directionCols = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < 8; i++) {
+            int newRow = row + directionRows[i];
+            int newCol = col + directionCols[i];
+
+            if (newRow >= 0 && newRow < 8 && newCol >= 0 && newCol < 8) {
+
+                Position newPos = new Position(newRow, newCol);
+                Piece targetPiece = board.getPieceFromPosition(newPos);
+
+                if (targetPiece == null || targetPiece.isWhite != this.isWhite) {
+                    moves.add(newPos);
+                    Log.e("PIECEE", newRow + "/" + newCol);
+                }
+            }
+        }
+
+        return moves.toArray(new Position[0]);
     }
 
     public boolean move(Board board, int index){
